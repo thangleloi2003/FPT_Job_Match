119cd5d932e0358265dea23014b6473d650fd33f
Thang Nguyen <thangleloi2003@gmail.com>
2024-10-25 16:33:20 +0700

    fix role

--- FPT_JobMatch/obj/Debug/netcoreapp3.1/FPT_JobMatch.AssemblyInfo.cs
+++ FPT_JobMatch/obj/Debug/netcoreapp3.1/FPT_JobMatch.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FPT_JobMatch")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd0716185a899e9299b76a8720d894ed29d24b79")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40ec5ccdd0259439b330daab16dac780df932dd2")]
 [assembly: System.Reflection.AssemblyProductAttribute("FPT_JobMatch")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FPT_JobMatch")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
